d1d834f76c35d27bd701fd6c8ccaa8c3d0942769
gabriel henrique <gabrielhenriqueas1@hotmail.com>
2024-08-18 19:46:10 -0300

    .

---
+++ Scripts Pythin/desafio0001.py
@@ -0,0 +1,9 @@
+#crie um script Python que leia o nome de
+#pessoa e mostre uma mensagem de boas-vindas
+#de acordo com o valor digitado
+
+nome = input("Qual é o seu nome? ")
+sobrenome  = input("Qual é o seu sobrenome")
+
+print("Boas-vindas", nome, sobrenome)
+
---
+++ Scripts Pythin/desafio003.py
@@ -0,0 +1,8 @@
+#crie um script Python que leia
+#dois números e tente mostrar a
+#soma entre eles
+
+n1 = int (input("Digite um número: "))
+n2 = int(input("Digite o segundo número: "))
+
+print("A soma dos dois números é: ",n1 + n2)
